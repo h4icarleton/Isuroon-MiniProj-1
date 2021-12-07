211737286f7176c32ad1d23d1c929295708057ba
Darryl York III <yorkd@carleton.edu>
2021-12-06 19:54:59 -0600

    Dynamically adding cards to container

--- src/index.js
+++ src/index.js
@@ -14,11 +14,11 @@ let createCards= () => {// n = number of cards to create
     var container = document.getElementById("results_container");
     let newCards = info.reduce(reducer, '')
     console.log(newCards);
-    // container.innerHTML += ;
+    container.innerHTML += newCards;
 }
 
-const reducer = (addedCards, {name, creds, gender, location, tax1= '',tax2= '',tax3=''}) => addedCards + 
-        `<div id="card">` +
+const reducer = (addedCards, {name, creds, gender, location, tax1= '',tax2= '',tax3=''}, lUpdated) => addedCards + 
+        `<div class="card">` +
         `<h2 class="name">Dr: ${name}</h2>` + 
             `<h3 class="credentials">Credentials: ${creds}</h3>` +
             `<h3 class="gender">Gender: ${gender}</h3>` +
