a7641ff843ecaa5083f42032186c47cf21b45f89
RichartE <>
2021-12-23 20:14:08 -0500

    Added python script to build multilingual html

---
+++ build.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+from translation_dictionary import translations
+import shutil, os
+
+SRC = "templates"
+DST_EN = "en"
+DST_SO = "so"
+
+shutil.rmtree(DST_EN)
+shutil.rmtree(DST_SO)
+
+shutil.copytree(SRC, DST_EN)
+shutil.copytree(SRC, DST_SO)
+
+for root, _, filename in os.walk(DST_SO):
+    for html_file in [f for f in filename if f.endswith(".html")]:
+        input = open(os.path.join(root, html_file), "r")
+        contents = input.read()
+        input.close()
+        soup = BeautifulSoup(contents, "html.parser")
+        soup.find("html")["lang"] = "so"
+        tags = soup.find_all(attrs={"tr": True})
+        for tag in tags:
+            if tag["tr"] in translations:
+                tag.string = translations[tag["tr"]]["so"]
+                del tag["tr"]
+            else:
+                print(f"Could not find translations for code")
+        output = open(os.path.join(root, html_file), "w")
+        output.write(str(soup))
+        output.close()
+
+for root, _, filename in os.walk(DST_EN):
+    for html_file in [f for f in filename if f.endswith(".html")]:
+        input = open(os.path.join(root, html_file), "r")
+        contents = input.read()
+        input.close()
+        soup = BeautifulSoup(contents, "html.parser")
+        tags = soup.find_all(attrs={"tr": True})
+        for tag in tags:
+            if tag["tr"] in translations:
+                tag.string = translations[tag["tr"]]["en"]
+                del tag["tr"]
+            else:
+                print(f"Could not find translations for code: {tag['tr']}")
+        output = open(os.path.join(root, html_file), "w")
+        output.write(str(soup))
+        output.close()
---
+++ translation_dictionary.py
@@ -0,0 +1,4 @@
+translations = {
+    "hw": {"en": "Hello World", "so": "Haye Aduunyo"},
+    "fn": {"en": "First Name", "so": "First Name 2"},
+}
