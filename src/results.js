5c12e9c12b7930385fc47283492fa9624c91c3b0
RichartE <>
2021-12-28 17:37:29 -0500

    Clean up

--- src/results.js
+++ src/results.js
@@ -2,7 +2,8 @@ window.onload = initialize;
 
 
 function initialize() {
-  document.getElementById('back-button').addEventListener('click', onBackButtonPress);
+    document.getElementById('back-button').addEventListener('click', onBackButtonPress);
+    createCards();
 }
 
 
@@ -11,32 +12,32 @@ let onBackButtonPress = () => location.href = "index.html";
 
 
 // RESULTS PAGE SCRIPTS
-let info =[
-    {name:'John Doe' , creds:'M.D.', gender:'F', location:'NE', tax1:'pediactrics', tax2:'family medicine', tax3:'chiropractor', lUpdated: Date.now()},
-    {name:'John Doe' , creds:'M.D.', gender:'F', location:'NE', tax1:'pediactrics', tax2:'family medicine', tax3:'chiropractor', lUpdated: Date.now()},
-    {name:'John Doe' , creds:'M.D.', gender:'F', location:'NE', tax1:'pediactrics', tax2:'family medicine', tax3:'chiropractor', lUpdated: Date.now()}
+let info = [
+    { name: 'John Doe', creds: 'M.D.', gender: 'F', location: 'NE', tax1: 'pediactrics', tax2: 'family medicine', tax3: 'chiropractor', lUpdated: Date.now() },
+    { name: 'John Doe', creds: 'M.D.', gender: 'F', location: 'NE', tax1: 'pediactrics', tax2: 'family medicine', tax3: 'chiropractor', lUpdated: Date.now() },
+    { name: 'John Doe', creds: 'M.D.', gender: 'F', location: 'NE', tax1: 'pediactrics', tax2: 'family medicine', tax3: 'chiropractor', lUpdated: Date.now() }
 ]; // Query/format doctor information into this variable here
 
 
 // Not working yet
-let createCards= () => {// n = number of cards to create
+let createCards = () => {// n = number of cards to create
     var container = document.getElementById("results_container");
     let newCards = info.reduce(reducer, '')
     console.log(newCards);
     container.innerHTML += newCards;
 }
 
-const reducer = (addedCards, {name, creds, gender, location, tax1= '',tax2= '',tax3=''}, lUpdated) => addedCards + 
-        `<div class="card">` +
-        `<h2 class="name">Dr: ${name}</h2>` + 
-            `<h3 class="credentials">Credentials: ${creds}</h3>` +
-            `<h3 class="gender">Gender: ${gender}</h3>` +
-            `<h3 class="location">Location: ${location}</h3>` +
-            `<ul><h3 class="TaxonomiesTitle">Taxonomies</h3>` +
-                `<li class="taxonomies 1">${tax1}</li>` +
-                `<li class="taxonomies 2">${tax2}</li>` +
-                `<li class="taxonomies 3">${tax3}</li>` +
-            `</ul>` +
-            `<h4 class="lastUpdated">Last Updated: ${lUpdated}</h4>` +
-            `<button type="button" class="reviews"><a href="review.html?name=Dr. ${name}">See Reviews</a></button>` +
-        `</div>`;
+const reducer = (addedCards, { name, creds, gender, location, tax1 = '', tax2 = '', tax3 = '' }, lUpdated) => addedCards +
+    `<div class="card">
+        <h2 class="name">Dr: ${name}</h2>
+        <h3 class="credentials">Credentials: ${creds}</h3>
+        <h3 class="gender">Gender: ${gender}</h3>
+        <h3 class="location">Location: ${location}</h3>
+        <ul><h3 class="TaxonomiesTitle">Taxonomies</h3>
+            <li class="taxonomies 1">${tax1}</li>
+            <li class="taxonomies 2">${tax2}</li>
+            <li class="taxonomies 3">${tax3}</li>
+        </ul>
+        <h4 class="lastUpdated">Last Updated: ${lUpdated}</h4>
+        <button type="button" class="reviews"><a href="review.html?name=Dr. ${name}">See Reviews</a></button>
+    </div>`;
